84c03c67d140667e5c6a5874e740877d013cd95f
James Murphy <jamesm8228@gmail.com>
2022-04-04 14:28:47 -0400

    Add refactor features

--- game.rb
+++ game.rb
@@ -9,19 +9,22 @@ class Game
 
   def rotate_turns
     @Player1.question
-    game_end
+    stats
+    scores
+    puts "----- NEW TURN -----"
     @Player2.question
-    game_end
+    stats
+    scores
     puts "----- NEW TURN -----"
     rotate_turns
   end
 
   def stats
-    puts "P1: #{@Player1.lives}/3 vs P2: #{@Player1.lives}/3"
+    puts "P1: #{@Player1.lives}/3 vs P2: #{@Player2.lives}/3"
   end
 
   def winner(name)
-    puts "#{name} wins with a score of #{name.lives}/3"
+    puts "#{name.name} wins with a score of #{name.lives}/3"
     puts "----- GAME OVER -----"
     puts "Good bye!"
   end
@@ -29,13 +32,10 @@ class Game
 
   def scores
     if @Player1.is_loser?
-      puts "#{@Player2.name} wins with a score of #{@Player2.lives}/3"
-      puts "----- GAME OVER -----"
-      puts "Good bye!"
-     elsif @Player2.is_loser?
-      puts "#{@Player1.name} wins with a score of #{@Player1.lives}/3"
-      
-     end
+      winner(@Player2)
+    elsif @Player2.is_loser?
+      winner(@Player1)
+    end
   end
 
   def start_game
